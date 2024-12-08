17952aa45c898768a52b6f243cc3224afd88769b
Hennryx Samson <hennryx101@gmail.com>
2024-12-09 05:01:56 +0800

    updating material

--- src/app/pages/views/admin/materials-documents/materials-documents.component.ts
+++ src/app/pages/views/admin/materials-documents/materials-documents.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalComponent } from "./modal/modal.component";
+import { RestApiService } from '../../../../services/api/rest-api.service';
 
 @Component({
     selector: 'app-materials-documents',
@@ -10,7 +11,7 @@ import { ModalComponent } from "./modal/modal.component";
     templateUrl: './materials-documents.component.html',
     styleUrl: './materials-documents.component.css'
 })
-export class MaterialsDocumentsComponent {
+export class MaterialsDocumentsComponent implements OnInit {
     filteredSchedules: any = [];
     isToggleActionsOpen: boolean = false;
     filteredMaterials: any = [];
@@ -19,12 +20,28 @@ export class MaterialsDocumentsComponent {
     isEditModalOpen: boolean = false;
     isFormModalOpen: boolean = false;
 
+    constructor(private restApi: RestApiService) {}
+    ngOnInit(): void {
+        this.restApi.get('get-all-material').subscribe({
+            next: (response) => {
+                this.filteredMaterials = response;
+                console.log(this.filteredMaterials);
+                
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        })
+        
+    }
+
     handleAttendanceActions(value: any) {
         const data = {
             status: value.action === 'present' ? true : false,
             scheduleid: value.id
         }
     }
+
     handleFormModal() {
         this.isFormModalOpen = !this.isFormModalOpen;
     }
