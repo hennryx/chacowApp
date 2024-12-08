c8c9bcf2170ad6a6448fe390a6196b68f47b4102
Dan Rosete <breedcrumxx@gmail.com>
2024-12-09 05:09:45 +0800

    v0.1.5

--- src/app/pages/views/admin/research-paper/research-paper.component.ts
+++ src/app/pages/views/admin/research-paper/research-paper.component.ts
@@ -1,50 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ModalComponent } from "./modal/modal.component";
+import { ModalComponent } from './modal/modal.component';
 import { RestApiService } from '../../../../services/api/rest-api.service';
 
 @Component({
-    selector: 'app-research-paper',
-    standalone: true,
-    imports: [CommonModule, FormsModule, ModalComponent],
-    templateUrl: './research-paper.component.html',
-    styleUrl: './research-paper.component.css'
+  selector: 'app-research-paper',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ModalComponent],
+  templateUrl: './research-paper.component.html',
+  styleUrl: './research-paper.component.css',
 })
 export class ResearchPaperComponent implements OnInit {
-    filteredResearch: any = [];
-    selectedResearch: any = null;
-    isEditModalOpen: boolean = false;
-    isFormModalOpen: boolean = false;
+  filteredResearch: any = [];
+  selectedResearch: any = null;
+  isEditModalOpen: boolean = false;
+  isFormModalOpen: boolean = false;
 
+  constructor(private restApi: RestApiService) {}
+  ngOnInit(): void {
+    this.handleGetResearch();
+  }
 
-    constructor(private restApi: RestApiService) { }
-    ngOnInit(): void {
-        this.handleGetResearch();
-    }
+  openEditModal(research: any) {
+    this.isEditModalOpen = true;
+    this.selectedResearch = research;
+  }
 
-    openEditModal(research: any) {
-        this.isEditModalOpen = true;
-    }
+  deleteAction(id: number) {
+    fetch(`http://localhost:3000/api/v1/paper/delete/${id}`, {
+      method: 'delete',
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }
 
-    deleteAction(id: number) {
+  handleFormModal() {
+    this.isFormModalOpen = !this.isFormModalOpen;
+  }
 
-    }
-
-    handleFormModal() {
-        this.isFormModalOpen = !this.isFormModalOpen;
-    }
-
-    handleGetResearch() {
-        /* this.restApi.get('').subscribe({
-          next: (response) => {
-              this.filteredResearch = response;
-              console.log(response);
-          },
-          error: (err) => {
-              console.error('get Research failed:', err);
-          }
-      }); */
-    }
-
+  handleGetResearch() {
+    this.restApi.get('paper/all').subscribe({
+      next: (response) => {
+        this.filteredResearch = response;
+        console.log(response);
+      },
+      error: (err) => {
+        console.error('get Research failed:', err);
+      },
+    });
+  }
 }
