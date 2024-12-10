01d6647b934f2e3f3a8a82ac7fc8160417276ca4
Hennryx Samson <hennryx101@gmail.com>
2024-12-10 23:43:00 +0800

    updated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,3 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
 import { RegisterComponent } from '../../components/register/register.component';
@@ -7,7 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage, LoginComponent, RegisterComponent, CommonModule],
+  imports: [LoginComponent, RegisterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
